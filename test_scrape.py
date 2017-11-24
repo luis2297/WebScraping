991b28110a4f32de589fe37c6f6abad87bfd7878
luisC <lucaesca@gmail.com>
2017-11-23 22:55:30 -0600

    cambio test

--- test_scrape.py
+++ test_scrape.py
@@ -13,23 +13,23 @@ class travis(unittest.TestCase):
 
 
     def test_select_fromBand(self):
-        self.assertIsNot(selectFromBand(''),'nombre:cualquiera,country:chicago')
+        self.assertIsNot(selectFromBand(''),'Name: Cristoffer Matlak ,Band:Jhator,Location: Alingsås,Status: Active,Formed In: N/A,Genre: Death n Roll')
 
     def test_select_fromDiscografia(self):
-        self.assertIsNot(selectFromDiscography(''),'nombre:cualquiera,country:chicago')
+        self.assertIsNot(selectFromDiscography(''),'Name: Christer "Grendel" Olsson,Country: Sweden,Location: N/A,Status: Unknown,Genre: Atmospheric Black Metal')
 
     def test_select_fromMember(self):
-        self.assertIsNot(selectFromMember(''),'nombre:cualquiera,country:chicago')
+        self.assertIsNot(selectFromMember(''),'Name: 12 Gauge Dead,Country: Sweden,Location: Linköping,Status: Split-up,Formed In: 2004,Years Active: 2004-2007')
 
     def test_deletes(self):
-        self.assertIsNone(deleteFromMember('iron maiden'))
-        self.assertIsNone(deleteFromDiscography('iron maiden'))
-        self.assertIsNone(deleteFromBand('iron maiden'))
+        self.assertIsNone(deleteFromMember(' 1 iron maiden'))
+        self.assertIsNone(deleteFromDiscography(' 2 Ton Predator'))
+        self.assertIsNone(deleteFromBand(' 2 Ton Predator'))
 
     def test_updates(self):
-        self.assertIsNone(updateMemberName('iron maiden','iron maiden'))
-        self.assertIsNone(updateDiscographyName('iron maiden','iron maiden'))
-        self.assertIsNone(updateBandName('iron maiden','iron maiden'))
+        self.assertIsNone(updateMemberName(' Christer "Grendel" Olsson',' Cristoffer Matlak'))
+        self.assertIsNone(updateDiscographyName(' Magnus Nilsson',' Tobbe'))
+        self.assertIsNone(updateBandName('Ton Predator','Ton Predator'))
         
             
 if __name__ =='__main__':
